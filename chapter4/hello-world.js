97ded0799c1bbe84c9a3a6d5c41c37c2c18cc6cd
Fengda HUANG <h@phodal.com>
2015-10-21 15:18:00 +0800

    add node.js hello world

---
+++ chapter4/hello-world.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('{"led": true}');
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
