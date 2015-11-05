514ebd85026e94e8e1433d01ad2baa03d6236794
Fengda HUANG <h@phodal.com>
2015-11-05 22:32:51 +0800

    change for localhost

--- chapter5/python-serail.py
+++ chapter5/python-serail.py
@@ -3,17 +3,15 @@ import urllib2
 import serial
 import time
 
-url = "http://api.designiot.cn/led.json"
+url = "http://localhost:3000/api/1"
+ser = serial.Serial("/dev/cu.usbmodem1451", 9600)
 
 while 1:
-    try:
-        results = urllib2.urlopen(url)
-        status = json.loads(results)['led']
-        ser = serial.Serial("/dev/cu.usbmodem1451", 9600)
-        if status:
-            ser.write('1')
-        else:
-            ser.write('0')
-        time.sleep(100)
-    except urllib2.URLError:
-        print "Bad URL or timeout"
+    date = urllib2.urlopen(url)
+    status = json.load(date)[0]['led']
+    print status
+    if status:
+        ser.write('1')
+    else:
+        ser.write('0')
+    time.sleep(1)
