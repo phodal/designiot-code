20a4ef5b164f4f0b91d9de6834ff86767a518273
Fengda HUANG <h@phodal.com>
2015-11-05 22:17:25 +0800

    format code

--- chapter5/python-serail.py
+++ chapter5/python-serail.py
@@ -7,13 +7,12 @@ url = "http://api.designiot.cn/led.json"
 
 while 1:
     try:
-        date = urllib2.urlopen(url)
-        result = json.load(date)
-        status = result['led']
-        ser = serial.Serial("/dev/ttyACM0", 9600)
-        if status == 1:
+        results = urllib2.urlopen(url)
+        status = json.loads(results)['led']
+        ser = serial.Serial("/dev/cu.usbmodem1451", 9600)
+        if status:
             ser.write('1')
-        elif status == 0:
+        else:
             ser.write('0')
         time.sleep(100)
     except urllib2.URLError:
