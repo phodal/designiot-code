e6a8f599514aa7b0a41625a72aa497ab5462065f
Fengda HUANG <h@phodal.com>
2015-11-10 09:02:11 +0800

    add jquery & highcharts & highcharts example

---
+++ chapter6/rest/public/scripts/highcharts-example.js
@@ -0,0 +1,39 @@
+$(function () {
+    $('#container').highcharts({
+        title: {
+            text: '月平均气温',
+            x: -20 //center
+        },
+        subtitle: {
+            text: '来源: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Today',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
+    });
+});
