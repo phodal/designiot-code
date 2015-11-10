374aa4e7540aae4a4c9bde27a3a226428bad9465
Fengda HUANG <h@phodal.com>
2015-11-10 15:31:58 +0800

    allow cors

--- chapter6/rest/app.js
+++ chapter6/rest/app.js
@@ -13,6 +13,12 @@ app.use(express.static(path.join(__dirname + '/', 'public')));
 app.set('views', path.join(__dirname + '/', 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function (req, res) {
     'use strict';
     res.render('index', {
