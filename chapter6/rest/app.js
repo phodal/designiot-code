6a2f51fbd7e45555163ed68f598b1b73e5f64de0
Fengda HUANG <h@phodal.com>
2015-11-12 11:54:28 +0800

    focues on chapter 6 code only

--- chapter6/rest/app.js
+++ chapter6/rest/app.js
@@ -33,38 +33,6 @@ app.get('/dashboard', function (req, res) {
     });
 });
 
-app.post('/', function (req, res) {
-    var userID = req.body.user;
-
-    var payload = {user: userID};
-    var led = req.body.led === "on";
-
-    var data = {user: userID, led: led};
-    db.find(payload, function (results) {
-        if (results.length > 0) {
-            db.update(data);
-            res.send({db: "update"});
-        } else {
-            db.insert(data);
-            res.send({db: "insert"});
-        }
-    });
-});
-
-app.get('/api/', function (req, res) {
-    var payload = {};
-    db.find(payload, function (results) {
-        return res.json(results);
-    });
-});
-
-app.get('/api/:user_id', function (req, res) {
-    var payload = {user: req.params.user_id};
-    db.find(payload, function (results) {
-        return res.json(results);
-    });
-});
-
 app.get('/api/:user_id/devices/:device_id/result/:result_id', function (req, res) {
     var payload = {user: req.params.user_id, device: req.params.device_id};
     db.findOrder(payload, parseInt(req.params.result_id),  function (results) {
@@ -86,36 +54,6 @@ app.post('/api/:user_id/devices/:device_id', function (req, res) {
 
     db.insert(data);
     res.send({db: "insert"});
-});
-
-function updateData(req, res) {
-    var userId = req.params.user_id;
-    var payload = {user: userId};
-
-    var data = req.body;
-    data.user = userId;
-
-    db.find(payload, function (results) {
-        if (results.length > 0) {
-            db.update(data);
-            res.send({db: "update"});
-        } else {
-            db.insert(data);
-            res.send({db: "insert"});
-        }
-    });
-}
-
-app.post('/api/:user_id', function (req, res) {
-    updateData(req, res);
-});
-
-app.put('/api/:user_id', function (req, res) {
-    updateData(req, res);
-});
-
-app.delete('/api/:user_id', function (req, res) {
-    res.send({});
 });
 
 app.listen(3000, function () {
