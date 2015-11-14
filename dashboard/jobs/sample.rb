b54155b4630d00573e437d8abf3c127c71317c25
Fengda HUANG <h@phodal.com>
2015-11-14 14:56:49 +0800

    remove usused fileds

--- dashboard/jobs/sample.rb
+++
@@ -1,9 +0,0 @@
-current_valuation = 0
-current_karma = 0
-
-SCHEDULER.every '2s' do
-  last_valuation = current_valuation
-  current_valuation = 20
-
-  send_event('valuation', { current: current_valuation, last: last_valuation })
-end
