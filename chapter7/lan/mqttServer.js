4bc7726b8d27271d177f281e751e82b2c732a00b
Fengda HUANG <h@phodal.com>
2015-12-08 07:25:55 +0800

    use offical demo

--- chapter7/lan/mqttServer.js
+++ chapter7/lan/mqttServer.js
@@ -1,36 +1,64 @@
 module.exports = function (client) {
+    var self = this;
+
+    if (!self.clients) self.clients = {};
+
     client.on('connect', function (packet) {
-        console.log(packet);
+        self.clients[packet.clientId] = client;
+        client.id = packet.clientId;
+        console.log("CONNECT: client id: " + client.id);
+        client.subscriptions = [];
         client.connack({returnCode: 0});
     });
 
     client.on('subscribe', function (packet) {
-        console.log(packet);
-        client.suback('subscribe');
-    });
+        var granted = [];
 
-    client.on('message', function (topic, message) {
-        console.log(message.toString());
+        console.log("SUBSCRIBE(%s): %j", client.id, packet);
+
+        for (var i = 0; i < packet.subscriptions.length; i++) {
+            var qos = packet.subscriptions[i].qos
+                , topic = packet.subscriptions[i].topic
+                , reg = new RegExp(topic.replace('+', '[^\/]+').replace('#', '.+') + '$');
+
+            granted.push(qos);
+            client.subscriptions.push(reg);
+        }
+
+        client.suback({messageId: packet.messageId, granted: granted});
     });
 
     client.on('publish', function (packet) {
-        console.log(packet);
+        console.log("PUBLISH(%s): %j", client.id, packet);
+        for (var k in self.clients) {
+            var c = self.clients[k];
+
+            for (var i = 0; i < c.subscriptions.length; i++) {
+                var s = c.subscriptions[i];
+
+                if (s.test(packet.topic)) {
+                    c.publish({topic: packet.topic, payload: packet.payload});
+                    break;
+                }
+            }
+        }
     });
+
     client.on('pingreq', function (packet) {
-        return client.pingresp();
+        console.log('PINGREQ(%s)', client.id);
+        client.pingresp();
     });
-    client.on('disconnect', function () {
-        return client.stream.end();
+
+    client.on('disconnect', function (packet) {
+        client.stream.end();
     });
-    client.on('error', function (error) {
-        return client.stream.end();
+
+    client.on('close', function (packet) {
+        delete self.clients[client.id];
     });
-    client.on('close', function (err) {
-        return "";
-    });
-    return client.on('unsubscribe', function (packet) {
-        return client.unsuback({
-            messageId: packet.messageId
-        });
+
+    client.on('error', function (e) {
+        client.stream.end();
+        console.log(e);
     });
 };
