e667f1f29f7e65e5a9b9338086335a0665df5503
Fengda HUANG <h@phodal.com>
2015-12-09 09:03:10 +0800

    add req url parse

--- chapter7/lan/coapServer.js
+++ chapter7/lan/coapServer.js
@@ -2,6 +2,23 @@ var Database = require('./db');
 var db = new Database();
 
 module.exports = function (req, res) {
+    var errorHandle = function () {
+        res.code = '4.00';
+        res.end(JSON.stringify({method: 'not support'}));
+    };
+
+    var uriQuery = [];
+    var existBlock = false;
+    for (var i = 1; i < req.options.length; i++) {
+        if (req.options[i].name === 'Uri-Query') {
+            uriQuery.push(req.options[i].value.toString());
+            existBlock = true;
+        }
+    }
+    if (!existBlock) {
+        return errorHandle();
+    }
+
     var handlerGet = function () {
         var payload = {user: 1, device: 1};
         db.findOrder(payload, 1, function (dbResult) {
@@ -21,11 +38,6 @@ module.exports = function (req, res) {
         res.end(JSON.stringify({method: 'post'}));
     };
 
-    var other = function () {
-        res.code = '4.00';
-        res.end(JSON.stringify({method: 'not support'}));
-    };
-
     switch (req.method) {
         case 'GET':
             handlerGet();
@@ -37,6 +49,6 @@ module.exports = function (req, res) {
             handPost();
             break;
         default:
-            return other();
+            return errorHandle();
     }
 };
